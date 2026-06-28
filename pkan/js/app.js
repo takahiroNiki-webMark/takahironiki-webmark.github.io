6406159829e83e07d412919407130eefbf2f3eac
takahiroNiki <57854048+takahiroNiki-webMark@users.noreply.github.com>
2026-06-29 00:11:55 +0900

    Update imports to use external URLs

--- pkan/js/app.js
+++ pkan/js/app.js
@@ -10,14 +10,14 @@ import {
   deleteTask,
   createProject,
   createTask
-} from "./state.js";
+} from "https://takahironiki-webmark.github.io/pkan/js/state.js";
 
 import {
   dom,
   initDom,
   renderProjectList,
   renderDetail
-} from "./ui.js";
+} from "https://takahironiki-webmark.github.io/pkan/js/ui.js";
 
 async function init() {
   await loadData();
